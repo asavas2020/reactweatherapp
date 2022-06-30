7f57871209bdb0513b5c828b30adebd10f387d86
Gokhan <gknsvsdata@gmail.com>
2022-06-30 23:58:22 +0300

    last commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Footer from "./companent/Footer";
+import Header from "./companent/Header";
+import Main from "./companent/Main";
+
+function App() {
+  return (
+    <>
+    <Footer/>
+    <Main/>
+    <Header/>
+    </>
+  )
+  
+}
+
+export default App;
